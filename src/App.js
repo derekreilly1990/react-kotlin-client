0cf624fa23bdd49488e7f72e69280a0b002ef696
Derek Reilly <derekreilly1990@gmail.com>
2021-08-23 14:29:22 +0200

    v1 react-Kotlin-frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [coffeeShops, setCoffeeShops] = useState([]);
+
+  useEffect( () => {
+    async function fetchData() {
+      const response = await fetch('/api/coffeeshops')
+      const body = await response.json()
+      setCoffeeShops(body._embedded.coffeeshops)
+      setIsLoading(false)
+    }
+    fetchData();
+
+
+
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <div className="App-intro">
+            <h2>Coffee Shop List</h2>
+            {coffeeShops.map(coffeeShop =>
+                <div key={coffeeShop.id}>
+                  {coffeeShop.name} - {coffeeShop.address}
+                </div>
+            )}
+          </div>
+        </header>
+      </div>
   );
 }
 
